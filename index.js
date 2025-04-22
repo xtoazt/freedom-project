3dd3d03ec0349d30a76a2a25d5e3c3ff6a6284ce
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-04-22 16:50:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://shuttleproxy.com/';
+const nggUrl = 'https://math-lessons.space/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'browser.lol') {
+    if (req.headers.host === 'math-lessons.space') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
