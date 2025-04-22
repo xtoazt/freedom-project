eca2cb342e3dbf80840dbc1b4363eecff0080ccb
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-04-22 09:34:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://browser.lol/create/';
+const nggUrl = 'https://shuttleproxy.com/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
